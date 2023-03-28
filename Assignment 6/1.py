ae6780c04d8bc5a02d91dc0494212ada2b3bdbc4
rodneydsouza <97938183+rodneydsouza@users.noreply.github.com>
2023-03-28 10:10:38 +0530

    Add files via upload

---
+++ Assignment 6/1.py
@@ -0,0 +1,8 @@
+# 1. Write a Python program to find those numbers which are divisible by 7 and multiple of 5,
+#  between 1500 and 2700 (both included)
+
+n = []
+for x in range(1500, 2701):
+    if (x%7==0) and (x%5==0):
+        n.append(str(x))
+print (','.join(n))
